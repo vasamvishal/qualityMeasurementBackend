b6cf2821c9a90158272f865d651d95cc0f790b46
vishalvasam <VishalVasam12>
2020-01-11 17:02:55 +0530

    REFACTORED:Code to make generic code

--- Utlity/UnitTypes.js
+++ Utlity/UnitTypes.js
@@ -1,35 +1,32 @@
 
-class UnitTypes {
- convert(body,callback){
-     if(body.Measurements=='LENGTH')
-     {
-         let length=[
-             { values: 1, Type: "INCHES" },
-             { values: 12, Type: "FEET" },
-             { values: 36, Type: "YARD" },
-             { values: 0.393701, Type: "CENTIMETER" }
-         ]
-         console.log(length)
-         let values1=length.find(o=>o.Type==body.convertTo).values;
-         let values2=length.find(o=>o.Type==body.Unit).values*body.Values;
-         let req={values1,values2};
-         return callback(null,req);
-     }
 
-     else if(body.Measurements=='VOLUME')
-     {
-         const VOLUME=[
-              { values: "3780", Type: "GALLON" },
-              { values: "1000", Type: "LITRES" },
-              { values: "1", Type: "MILLILITRES" }
-         ]
-         console.log(VOLUME)
-         let values1=VOLUME.find(o=>o.Type==body.convertTo).values;
-         let values2=VOLUME.find(o=>o.Type==body.Unit).values*body.Values;
-         let req={values1,values2};
-         return callback(null,req);
-     }
-}
+class UnitTypes {
+    convertion = (body, callback) => {
+        let values1=body.array.find(o=>o.Type==body.body.convertTo).values;
+        let values2=body.array.find(o=>o.Type==body.body.Unit).values*body.body.Values;
+        let req = {values1, values2};
+        return callback(null, req);
+    };
+    convert=(body, callback) =>{
+        if (body.Measurements == 'LENGTH') {
+            let array = [
+                {values: 1, Type: "INCHES"},
+                {values: 12, Type: "FEET"},
+                {values: 36, Type: "YARD"},
+                {values: 0.393701, Type: "CENTIMETER"}
+            ];
+            let req={array,body}
+            this.convertion(req,callback);
+        } else if (body.Measurements == 'VOLUME') {
+            const array = [
+                {values: "3780", Type: "GALLON"},
+                {values: "1000", Type: "LITRES"},
+                {values: "1", Type: "MILLILITRES"}
+            ];
+            let req={array,body}
+            this.convertion(req,callback);
+        }
+    }
 }
 
 module.exports = new UnitTypes();
